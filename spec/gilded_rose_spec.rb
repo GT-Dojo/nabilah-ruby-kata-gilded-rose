052cc7eac563b66e8339889fc34a06651d95f027
Nabilah B <nabilah_banu@tech.gov.sg>
2023-10-10 21:53:25 +0800

    Added another test

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,11 +3,19 @@
 require './app/gilded_rose'
 
 describe GildedRose do
+
   describe '#update_quality' do
     it 'does not change the name' do
       items = [Item.new('foo', 0, 0)]
       described_class.new(items).update_quality
       expect(items[0].name).to eq 'foo'
     end
+
+    it 'sellIn date has passed, quality degrades twice as fast' do
+      items = [Item.new('foo', -1, 100)]
+      described_class.new(items).update_quality
+      expect(items[0].quality).to eq 98
+    end
+
   end
 end
