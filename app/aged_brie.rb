346fa372544ef797501f208b9d43a3b717fb6e97
Nabilah B <nabilah_banu@tech.gov.sg>
2023-10-12 23:26:24 +0800

    Refactor aged brie

--- app/aged_brie.rb
+++ app/aged_brie.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 require './app/item'
-class Aged_Brie < Item
+class AgedBrie < Item
   def initialize(sell_in, quality)
     @name = 'Aged Brie'
     @sell_in = sell_in
@@ -8,13 +8,9 @@ class Aged_Brie < Item
   end
   def update
     @sell_in -= 1
-    if @quality < 50
-      @quality += 1
-    end
+    @quality += 1
+    @quality += 1 if @sell_in < 0
+    @quality = 50 if @quality > 50
+
   end
 end
-
-# ab = Aged_Brie.new(2,48)
-# ab.update
-# puts ab.to_s
-
